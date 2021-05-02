6a1dc1ab69fe7dd6f1befa4de94a8721359f887b
pkjGIt <pradyumna.hyd@gmail.com>
2021-05-02 11:16:41 +0530

    Initial commit

---
+++ venv/file1.py
@@ -0,0 +1,21 @@
+'''
+print ("hi")
+x = "1234569"
+y = input("enter a number")
+p = "my number{}".format(y)
+print (p)
+
+a = ['pradyumna','kumar', 'jena']
+print("".join(a))
+
+import os
+import socket
+print (os)
+print (socket)
+'''
+class cls:
+    ppp,kk,jj = range(3)
+obj=cls()
+print(obj.ppp)
+print(cls.kk)
+print(cls.jj)
